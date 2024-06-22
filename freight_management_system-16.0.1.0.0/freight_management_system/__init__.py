9c3a894086d8538898fe0a3e9e80cf8ad53be154
mosabomer5 <mosablink97@email.com>
2024-06-22 17:28:17 +0200

    hadeel test18

--- freight_management_system-16.0.1.0.0/freight_management_system/__init__.py
+++ freight_management_system-16.0.1.0.0/freight_management_system/__init__.py
@@ -22,4 +22,4 @@
 ###################################################################################
 from . import model
 from . import wizard
-#hadeel project manager test8
+#hadeel project manager test9
