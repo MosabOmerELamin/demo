9a47fef622081227d36dcb0154f4f52e025e1aac
mosabomer5 <mosablink97@email.com>
2024-06-24 22:21:35 +0200

    Adding custom folder to the server test3

--- freight_management_system-16.0.1.0.0/freight_management_custom/__init__.py
+++ freight_management_system-16.0.1.0.0/freight_management_custom/__init__.py
@@ -20,6 +20,4 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 ###################################################################################
-from . import model
-from . import wizard
-#hadeel project manager test12
+
